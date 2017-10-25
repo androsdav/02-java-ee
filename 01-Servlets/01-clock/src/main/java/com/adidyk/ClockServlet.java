86d6607cd0594c6c4889553204bdd2831fece64d
androsdav <androsdav@gmail.com>
2017-10-25 21:55:20 +0300

    update

---
+++ 01-Servlets/01-clock/src/main/java/com/adidyk/ClockServlet.java
@@ -0,0 +1,18 @@
+package com.adidyk;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class ClockServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Current time is " + new Date());
+    }
+}
