d57f26629a8de5b5df4f798163c85d079cef17e7
androsdav <androsdav@gmail.com>
2019-06-04 02:03:02 +0300

    develop2

---
+++ Hibernate/01-hiber/src/main/java/com/adidyk/services/StartUi.java
@@ -0,0 +1,31 @@
+package com.adidyk.services;
+
+import com.adidyk.pojo.Developer;
+import com.adidyk.services.RunDeveloper;
+
+/**
+ * Class StartUi for create jar file and start program.
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 09.02.2019.
+ * @version 1.0.
+ */
+public class StartUi {
+
+    /**
+     * main - creates jar file and runs program.
+     * @param arg - is nothing.
+     */
+    public static void main(String[] arg) {
+        Developer user = new Developer(1, "andreyUpdate", "diduk", "tehnick", "1000");
+        System.out.println("test hibernate" + user);
+        RunDeveloper runDeveloper = new RunDeveloper();
+        //runDeveloper.addDeveloper(user);
+        //runDeveloper.updateDeveloper(1, "88888");
+        //runDeveloper.removeDeveloper(7);
+        for (Developer developer : runDeveloper.listDeveloper()) {
+            System.out.println(developer);
+        }
+        runDeveloper.removeDeveloper(4);
+    }
+
+}
